45626e4bcdb0943779809184dd232ed438316f86
Juan José Quiroz Omaña <juanjqogm@gmail.com>
2022-10-17 01:26:30 +0900

    Updated the config file.

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -8,7 +8,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'AISP: A Multi-Arm Science Platform for Artificial Intelligence Research',
   tagline: 'Murilo Marques Marinho, Juan José Quiroz-Omaña, and Kanako Harada.',
-  url: 'https://your-docusaurus-test-site.com',
+  url: 'https://github.com/AISciencePlatform/AISciencePlatform.github.io', 
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
@@ -16,8 +16,10 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'AISciencePlatform', // Usually your GitHub org/user name.
+  projectName: 'AISciencePlatform.github.io', // Usually your repo name.
+  deploymentBranch: 'gh-pages',
+  trailingSlash: false,
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
@@ -37,14 +39,14 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/AISciencePlatform/AISciencePlatform',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/AISciencePlatform/AISciencePlatform',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
