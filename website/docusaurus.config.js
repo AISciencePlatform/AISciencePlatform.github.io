4b37610b2ec0f7d7da6aa7cb172a0c8a6c071e1a
Juan José Quiroz Omaña <juanjqogm@gmail.com>
2022-10-21 17:21:58 +0900

    Added changes in the config file

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -16,7 +16,7 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'juanjqo', // Usually your GitHub org/user name.
+  organizationName: 'AISciencePlatform', // Usually your GitHub org/user name.
   projectName: 'AISciencePlatform.github.io', // Usually your repo name.
   deploymentBranch: 'gh-pages',
   trailingSlash: false,
