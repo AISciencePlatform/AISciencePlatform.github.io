58e0e1f19afc651009b4f79afd4311c7f4fa16aa
juancho <juanjqogm@gmail.com>
2022-10-21 21:11:34 +0900

    Deploy website - based on 80b8cfa1b4068061fa950e0fa1089c76ff2f5bc6

---
+++ assets/js/bd59732b.cea96cca.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkmy_website=self.webpackChunkmy_website||[]).push([[946],{594:t=>{t.exports=JSON.parse('{"title":"Simulations","slug":"/category/simulations","permalink":"/docs/category/simulations","navigation":{"previous":{"title":"Introduction","permalink":"/docs/intro"},"next":{"title":"Overall Strategy","permalink":"/docs/Simulations/overall-strategy"}}}')}}]);
